997753a479b37cbda538cde884734cecfbe2f83a
kuzkostia <112246522+kuzkostia@users.noreply.github.com>
2023-06-12 15:01:48 +0300

    Summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import { lazy, Suspense } from 'react'; // оновлюємо імпорти
+import { Toaster } from 'react-hot-toast';
+
+const SharedLayout = lazy(() => import('./SharedLayout/SharedLayout'));
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router>
+      {' '}
+      {/* Замінюємо <Routes> на <Router> */}
+      <Suspense fallback={<div>Loading...</div>}>
+        {' '}
+        {/* Додаємо <Suspense> */}
+        <SharedLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+
+            <Route path="movies/:movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </SharedLayout>
+      </Suspense>
+      <Toaster />
+    </Router>
   );
 };
