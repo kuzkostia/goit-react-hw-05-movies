f3e1a89cb1c5eccd45897ff3e2f01ba77cdb7ed1
kuzkostia <112246522+kuzkostia@users.noreply.github.com>
2023-06-16 16:33:44 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate} from 'react-router-dom';
 import SharedLayout from '../SharedLayout/SharedLayout';
 
 const Home = lazy(() => import('../../pages/Home/Home'));
@@ -20,6 +20,7 @@ export const App = () => {
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
+        <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
   );
